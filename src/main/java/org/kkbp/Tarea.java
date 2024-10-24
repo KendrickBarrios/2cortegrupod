2da7aeb547ddca399298d245ef09e961c55c07eb
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-10-24 17:44:10 -0600

    Creacion de entidad tarea

--- src/main/java/org/kkbp/Tarea.java
+++ src/main/java/org/kkbp/Tarea.java
@@ -1,5 +1,39 @@
 package org.kkbp;
 
-public class Tarea {
+import java.time.*;
 
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.*;
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Tarea {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@Hidden
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@Column(length = 6)
+	String tareaId;
+	@Column(length=30)
+	String nombreTarea;
+	@Column(length=255)
+	String descripcion;
+	boolean estado;
+	LocalDate fechaLimite;
+	@ManyToOne
+	(fetch=FetchType.LAZY,
+	optional=true)
+	@DescriptionsList
+	Empleado empleado;
+	
+	@ManyToOne
+	(fetch=FetchType.LAZY,
+	optional=true)
+	@DescriptionsList
+	Proyecto proyecto;
+	
 }
